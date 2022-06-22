c21a6c0963e2beeccae4390a44203c1adebe8092
김민준 <mjkim3092@gmail.com>
2022-06-22 15:47:16 +0900

    update

---
+++ pystudy_2/김민준.py
@@ -0,0 +1,30 @@
+score=int(input("fff"))
+
+if score == 100:
+    print("백점")
+elif 90 < score:
+    print("구십점")
+elif 80 < score:
+    print("팔십점")
+elif 70 < score:
+    print("칠십점")
+elif 60 < score:
+    print("육십점")
+elif 50 < score:
+    print("오십점")
+else:
+    print("접어라 그냥")
+
+alist = [10526, 10319, 11215, 10309]
+
+print(alist)
+if 10526 in alist:
+    print("최원서ㅋㅋ")
+
+if 10303 in alist
+    print("김가인 여기 없다")
+
+print(alist + [0])
+print(alist * 3)
+for element in alist:
+        print(element)
